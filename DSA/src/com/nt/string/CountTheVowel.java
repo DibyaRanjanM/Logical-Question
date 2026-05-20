a97c4db0acbb293879a00e4252eaefc9432de43c
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-05-20 12:39:37 +0530

    Vowel count

---
+++ DSA/src/com/nt/string/CountTheVowel.java
@@ -0,0 +1,20 @@
+package com.nt.string;
+/*
+ * Count the number of vowels in a string.
+ */
+
+public class CountTheVowel {
+	public static void main(String[] args) {
+		String str = "Dibya";
+		int count = 0;
+		for (int i = 0; i <= str.length() - 1; i++) {
+			// char ch = str.charAt(i);
+			if (str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O'
+					|| str.charAt(i) == 'U') {
+				count++;
+			}
+		}
+		System.out.println("Number of Vowel " + count);
+	}
+
+}
