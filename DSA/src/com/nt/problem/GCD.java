f57d4772c980c7bed1ff3a595a6b9e4bb2c2e0f0
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-07-20 15:08:26 +0530

    GCD

---
+++ DSA/src/com/nt/problem/GCD.java
@@ -0,0 +1,22 @@
+package com.nt.problem;
+
+public class GCD {
+	public static void main(String[] args) {
+		int a=48;
+		int b=18;
+		System.out.println(gcd(a, b));
+
+	}
+
+	public static int gcd(int a, int b) {
+		while (b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+
+		}
+		return a;
+
+	}
+
+}
