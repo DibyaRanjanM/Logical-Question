401ef529393e4840d55a4cd4d75e375cd5974405
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-08-12 13:26:01 +0530

    Linked list fetch element

---
+++ DSA/src/linkedlist/dsa/LinkedList.java
@@ -0,0 +1,10 @@
+package linkedlist.dsa;
+//Element 
+public class LinkedList {
+	public static void main(String[] args) {
+		int arr[] = { 2, 5, 5, 6 };
+		Node y = new Node(arr[3]);
+		System.out.println(y.data);
+	}
+
+}
