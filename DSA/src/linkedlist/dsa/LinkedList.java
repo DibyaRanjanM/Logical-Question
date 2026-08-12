af81a02e5a72ad289aa1ce32b285ff7e76049892
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-08-12 13:44:25 +0530

    xyz

--- DSA/src/linkedlist/dsa/LinkedList.java
+++ DSA/src/linkedlist/dsa/LinkedList.java
@@ -1,9 +1,10 @@
 package linkedlist.dsa;
+
 //Element 
 public class LinkedList {
 	public static void main(String[] args) {
 		int arr[] = { 2, 5, 5, 6 };
-		Node y = new Node(arr[3]);
+		Node y = new Node(arr[2]);
 		System.out.println(y.data);
 	}
 
