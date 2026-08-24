4cefc67046cef922215a8ba8eed0b9ebec5fa794
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-08-24 14:58:24 +0530

    insert value before tail

---
+++ DSA/src/com/nt/doublelinkedlist/insertValuebeforeTailofLL.java
@@ -0,0 +1,62 @@
+package com.nt.doublelinkedlist;
+
+//insert value before tail
+
+public class insertValuebeforeTailofLL {
+	public static void main(String[] args) {
+		Node head = new Node(2);
+		Node n2 = new Node(4);
+		Node n3 = new Node(5);
+		Node n4 = new Node(7);
+
+		head.next = n2;
+		n2.back = head;
+
+		n2.next = n3;
+		n3.back = n2;
+
+		n3.next = n4;
+		n4.back = n3;
+
+		System.out.println("Before insertion:");
+		print(head);
+
+		head = insertValuebeforeTailofLL(head, 1);
+
+		System.out.println("After insertion:");
+		print(head);
+
+	}
+
+	private static Node insertValuebeforeTailofLL(Node head, int val) {
+		if (head == null) {
+			return insertHead(head, val);
+
+		}
+		Node tail = head;
+		while (tail.next != null) {
+			tail = tail.next;
+		}
+		Node prev = tail.back;
+		Node newNode = new Node(val, tail, prev);
+		prev.next = newNode;
+		tail.back = newNode;
+		return head;
+
+	}
+
+	private static Node insertHead(Node head, int val) {
+		Node newhead = new Node(val, head, null);
+		head.back = newhead;
+		return newhead;
+
+	}
+
+	private static void print(Node head) {
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
+		}
+		System.out.println();
+	}
+}
