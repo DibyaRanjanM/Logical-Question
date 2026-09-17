be8b4bab4dd4e4966d825cc57b5c9efd2b5313d2
DibyaRanjan <mohantydibyaranjan090@gmail.com>
2026-09-17 07:34:55 +0530

    XYZ

--- DSA/src/com/nt/stream/EvenOrOdd.java
+++ DSA/src/com/nt/stream/EvenOrOdd.java
@@ -1,11 +1,23 @@
 package com.nt.stream;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class EvenOrOdd {
 	public static void main(String[] args) {
 		Stream<Integer> stream = Stream.of(2, 5, 6, 8, 9, 3);
-		stream.filter(p -> p % 2 == 0).forEach(System.out::println);
+		// stream.filter(p -> p % 2 == 0).forEach(System.out::println);
+
+//		System.out.println("Using Collectors.ToList");
+//		Set<Integer> collect = stream.filter(p -> p % 2 == 0).collect(Collectors.toSet());
+//		collect.forEach(System.out::println);
+
+		System.out.println("Set of Odd nyumber");
+		List<Integer> collect2 = stream.filter(p -> p % 2 == 1).collect(Collectors.toList());
+		collect2.forEach(System.out::println);
+
 	}
 
 }
